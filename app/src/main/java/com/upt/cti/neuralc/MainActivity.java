218b356b6007b3323c1002c0f5e5e68450e53cc7
Florin Hegedus <florinxhgds@gmail.com>
2021-12-17 17:20:38 +0200

    created XRays Button

--- app/src/main/java/com/upt/cti/neuralc/MainActivity.java
+++ app/src/main/java/com/upt/cti/neuralc/MainActivity.java
@@ -2,9 +2,12 @@ package com.upt.cti.neuralc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,6 +15,7 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     private static final int PICK_IMAGE = 100;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     private ImageView imageView;
 
     @Override
@@ -21,11 +25,19 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = (ImageView) findViewById(R.id.imageView);
         Button selectButton = (Button) findViewById(R.id.selectButton);
+        Button takePhotoButton = (Button) findViewById(R.id.takePhotoButton);
 
         selectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openGallery();
+            }
+        });
+
+        takePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
             }
         });
     }
@@ -35,12 +47,28 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(gallery, PICK_IMAGE);
     }
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
             Uri imageUri = data.getData();
             imageView.setImageURI(imageUri);
         }
     }
+
+
 }
